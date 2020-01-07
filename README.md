708f7a4533830b6cc6a50ec6f40d9e8ae02bc057
Pedro Zatta <pedrozatta@gmail.com>
2020-01-07 13:55:51 -0300

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# lince-commons-health-java
---
+++ src/main/java/work/lince/commons/health/controller/HealthController.java
@@ -0,0 +1,29 @@
+package work.lince.commons.health.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import work.lince.commons.health.model.Health;
+
+import java.security.Principal;
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping(path = {"/", "/health"}, produces = MediaType.APPLICATION_JSON_VALUE)
+public class HealthController {
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Health health(Principal principal) {
+        return Health.builder()
+                .status("ok")
+                .now(LocalDateTime.now())
+                .user(principal.getName())
+                .build();
+    }
+
+
+}
